7de6085155e6ca20219325e635769113f1fed27f
cbl-umces <lyubchic@umces.edu>
2016-01-20 23:16:09 -0500

    Initial commit.

---
+++ Rscript.R
@@ -0,0 +1,51 @@
+# Slava
+# This is an R script.
+
+
+
+
+# Anna
+
+
+
+
+# Caitlin
+
+
+
+
+# Daniel
+
+
+
+
+# Emma
+
+
+
+
+# Jacob
+
+
+
+
+# Juliet
+
+
+
+
+# Rachel
+
+
+
+
+# Sam
+
+
+
+
+# Yuanchao
+
+
+
+
