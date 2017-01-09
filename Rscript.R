c8795a41f394b50029dd951b43df3ed9c601de48
vlyubchich <lyubchic@umces.edu>
2017-01-09 09:40:40 -0500

    Added usernames

--- Rscript.R
+++ Rscript.R
@@ -1,80 +1,43 @@
-# Slava
-# This is an R script
-rnorm(12)  
-#Hello world.
-sqrt(pi)
-sdsdf
-
-#1
-
-
-
-
-#2
-
-
-
-
-#3
-
-
-
-
-#4
-
-
-
-
-#5
-
-
-
-
-#6
-
-
-
-
-#7
-
-
-
-
-#8
-
-
-
-
-#9
-
-
-
-
-#10
-
-
-
-
-#11
-
-
-
-
-#12
-
-
-
-
-#13
-
-
-
-
-#14
-
-
-
-
-#15
-
-
+# # Slava
+# # This is an R script
+# rnorm(12)  
+# #Hello world.
+# sqrt(pi)
+# 
+# klankowi
+# 
+# drosale1
+# 
+# 
+# Spitz190
+# 
+# 
+# williamsernest
+# 
+# 
+# saklick
+# 
+# 
+# sckuchinsky
+# 
+# 
+# 
+# spmoessinger
+# 
+# 
+# 
+# katdoering
+# 
+# 
+# keotas
+# 
+# 
+# dtizabi
+# 
+# 
+# 
+# ahotopp
+# 
+# 
+# 
+# Hanif82
